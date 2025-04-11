950ea864d64763e9ed4becebb71bbbbf4c334c6f
sirajali <sirajalipyt@gmail.com>
2025-04-11 19:24:04 +0500

    first commit

---
+++ projectreelit/app/tests.py
@@ -0,0 +1,56 @@
+#from django.test import TestCase
+
+# Create your tests here.
+from typing import List
+
+# some custom ideas for waaay later if u want
+
+# 1 customized communities like subreddits from reddit
+
+# 2 this from ai
+
+"""
+Weird Post Types
+Instead of just text/image/video, add quirky ones:
+
+"Noise Posts" post ambient sounds or random generated noises.
+
+"Hot Takes"  timed posts that disappear after 24 hours unless voted to stay.
+
+"Rant Mode"  users can write a post, but it gets posted with no edit or delete option.
+"""
+
+#3
+"""
+
+Leaderboard or "Hall of Reee"
+Showcase top posts or users each week.
+
+“Shameboard” for most downvoted posts with funny auto-generated insults.
+"""
+
+
+
+# design tips
+"""
+🧱 Design Polish Tips
+Microinteractions: Little animations when you vote, comment, or hover over stuff. Makes it feel alive.
+
+Custom Error Pages: “404  You screamed into the void. It screamed back.”
+
+Loading States: Skeleton screens or something goofy like a dancing emoji.
+
+Fun Copywriting: Inject personality into buttons and headers. Replace “Submit” with “Scream into the void”.
+
+
+"""
+
+
+
+
+
+
+
+
+
+
